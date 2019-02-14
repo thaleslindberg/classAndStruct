3fa43651105bea7106e9f370b319cb4006fdecc2
Thales <thales.lindberg@gmail.com>
2019-02-13 21:14:05 -0300

    changes

---
+++ Contents.swift
@@ -0,0 +1,42 @@
+
+/*
+ 
+ - having a class is like giving somebody access to edit and change anything
+ - classes cannot have inheritance
+ */
+
+class GoogleSheet {
+    
+    var message = String()
+    
+    init(message: String) {self.message = message}
+    
+}
+let newGoogleSheet = GoogleSheet(message: "editable file")
+let newGoogleSheetCopy = newGoogleSheet
+newGoogleSheetCopy.message = "permitted access to edit"
+
+print(newGoogleSheet.message)
+print(newGoogleSheetCopy.message)
+
+
+/*
+ 
+ - having structs the original file it ain't changed
+ - structs cannot have inheritance
+ */
+struct ExcelSpreadSheet {
+    
+    var file = String()
+    
+    init(file: String) { self.file = file}
+    
+}
+
+let newExcelSpreadSheet = ExcelSpreadSheet(file: "editable file too")
+var newExcelSpreadSheetCopy = newExcelSpreadSheet
+newExcelSpreadSheetCopy.file = "but its keeps the original copy"
+
+print(newExcelSpreadSheet.file)
+print(newExcelSpreadSheetCopy.file)
+
